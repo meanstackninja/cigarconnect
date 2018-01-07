8d3b0b1e360751c6c2fb6161f68008868ea62741
Salvatore Allegra <salallegra@gmail.com>
2018-01-07 04:40:48 -0800

    Refactor from object literals to object constructors

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-/* Create a cigar object */
+/* Create a cigar object constructor*/
 
-var cigar = {
-    brand: "Oliva",
-    type: "Serie V",
-    retailPrice: 9.99
-};
+function Cigar(brand,type,price){
+    this.brand = brand;
+    this.type = type;
+    this.price = price;
+}
 
-console.log(cigar["brand"]);
+function Location(streetNum,streetName,city,state,zip,phoneNumber,siteUrl,bLounge,bInetAccess){
+    this.streetNum = streetNum;
+    this.streetName = streetName;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.sitUrl = siteUrl;
+    this.bLounge = bLounge;
+    this.bInetAccess = bInetAccess;
+
+
+}
+
+
