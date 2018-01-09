fcd02af8a69913f248e5447f18a1525d57e60bca
Salvatore Allegra <salallegra@gmail.com>
2018-01-09 00:18:33 -0800

    Add chai assertion library and require in app-spec.js

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,10 +1,6 @@
-var assert = require('assert');
-var calculator = require('../app');
-describe('calculator', function() {
-    describe('add function', function() {
-        it('adds numbers', function () {
-            var result = calculator.add(1, 1);
-            assert.equal(result, 2);
-        });
-    });
+var expect = require("chai").expect;
+describe("printName()", function(){
+
+    it("Should print the last name first");
+
 });
