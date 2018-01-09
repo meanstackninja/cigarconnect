84f103f76137b95cfb3490282d882c98ed5d21d6
Salvatore Allegra <salallegra@gmail.com>
2018-01-09 04:05:37 -0800

    setup stub for test I'm going to write

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,6 +1,10 @@
 var expect = require("chai").expect;
+var tools = require("../lib/tools");
 describe("printName()", function(){
 
-    it("Should print the last name first");
+    it("Should print the last name first", function(){
+
+
+    });
 
 });
